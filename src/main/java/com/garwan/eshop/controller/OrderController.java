4955b866f99526a728ab1788cb238aae611fad68
Vladimir Lepen <vladimir.lepen@gmail.com>
2020-08-24 11:14:45 +0200

    Fixed REST url - using plural for resources - removed redundant ProductListController.java

--- src/main/java/com/garwan/eshop/controller/OrderController.java
+++ src/main/java/com/garwan/eshop/controller/OrderController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/order")
+@RequestMapping(value = "/orders")
 @Validated
 public class OrderController {
     private final OrderService orderService;
--- src/main/java/com/garwan/eshop/controller/ProductController.java
+++ src/main/java/com/garwan/eshop/controller/ProductController.java
@@ -1,18 +1,24 @@
 package com.garwan.eshop.controller;
 
+import com.garwan.eshop.model.Product;
 import com.garwan.eshop.model.ProductDetail;
 import com.garwan.eshop.service.ProductService;
+import java.math.BigDecimal;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/product")
+@RequestMapping(value = "/products")
 @Validated
 public class ProductController {
     private final ProductService productService;
@@ -24,5 +30,15 @@ public class ProductController {
     @GetMapping(value = "/{id}", produces = "application/json")
     public ResponseEntity<ProductDetail> getProduct(@NotNull @Positive @PathVariable Long id) {
         return ResponseEntity.of(productService.findById(id));
+    }
+
+    @GetMapping(produces = "application/json")
+    public ResponseEntity<Page<Product>> getProductsByFilter(
+        @NotNull @Positive @RequestParam("priceMin") BigDecimal priceMin,
+        @NotNull @Positive @RequestParam("priceMax") BigDecimal priceMax,
+        @NotBlank @RequestParam("namePart") String namePart,
+        Pageable pageable
+    ) {
+        return ResponseEntity.ok(productService.findByFilter(priceMin, priceMax, namePart, pageable));
     }
 }
